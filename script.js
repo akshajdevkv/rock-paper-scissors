315a40888607e4e2dd1dd1498b0b65fb1af8ac43
Akshaj Dev <akshajdevkv@gmail.com>
2025-04-13 17:29:47 +0530

    add endgame logic and ui

--- script.js
+++ script.js
@@ -16,28 +16,34 @@ function playRound(playerSelection) {
     let resultText = '';
     if (playerSelection === computerSelection) {
       resultText = "It's a draw!";
+        updateScore('draw')
     } else if (
       (playerSelection === 'rock' && computerSelection === 'scissors') ||
       (playerSelection === 'paper' && computerSelection === 'rock') ||
       (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
       resultText = `You win! ${playerSelection} beats ${computerSelection}`;
-        updateScore('player');
+        updateScore('player');displayRoundWinner('win')
     } 
     else {
         updateScore('computer');
+        displayRoundWinner('lose');
       resultText = `You lose! ${computerSelection} beats ${playerSelection}`;
     }
+    if(playerScore === 5 || computerScore === 5){
+     
+        endGame(playerScore===5);
+    }
   
-    document.getElementById('result').textContent = resultText;
+     
   }
 const buttons = document.querySelectorAll('button[data-choice]');
 buttons.forEach(button => {
 button.addEventListener('click', () => {
-    console.log("Hello");
+ 
     const userChoice = button.dataset.choice;
-    //  playRound(userChoice);
-    console.log(userChoice);
+     playRound(userChoice);
+  
     });
 }); 
  
@@ -53,5 +59,28 @@ button.addEventListener('click', () => {
     document.querySelector('#score').textContent =
       `Player: ${playerScore} | Computer: ${computerScore}`;
   }  
- 
- 
+ function endGame(winner){
+    const container = document.querySelector(".container");
+    container.innerHTML = '';
+    const title = document.createElement('h1');
+    title.textContent = winner? "You are the winner🎉 ":"Computer is the winner 💀";
+    container.appendChild(title);
+
+ }
+ function displayRoundWinner(result){
+    const para = document.querySelector("#result");
+    console.log(result);
+    switch(result){
+        case 'win':
+            para.textContent = "You Win 🎉";
+            break;
+        case 'lose':
+            para.textContent = "You Lose 💀";
+            break;
+        default:
+            para.textContent = "It's a draw 🤝";
+            break;
+    }
+     
+     
+ }
